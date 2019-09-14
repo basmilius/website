f5f5a4706cc5ba02d7aa62d009cf349fa9bf9e7d
Bas Milius <basmilius@hotmail.com>
2019-09-14 15:01:45 +0200

    Initial commit.

---
+++ src/Bas/Website/Router/RootRouter.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Copyright (c) 2019 - Bas Milius <bas@mili.us>
+ *
+ * This file is part of the Latte Framework package.
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Bas\Website\Router;
+
+use Columba\Http\ResponseCode;
+use Columba\Router\Route\AbstractRoute;
+use Columba\Router\Context;
+use Columba\Router\RouterException;
+use Columba\Util\StringUtil;
+use Latte\Core\Helper\Environment;
+use Latte\Core\Router\AbstractRouter;
+
+/**
+ * Class RootRouter
+ *
+ * @author Bas Milius <bas@mili.us>
+ * @package Bas\Website\Router
+ * @since 1.0.0
+ */
+final class RootRouter extends AbstractRouter
+{
+
+	/**
+	 * RootRouter constructor.
+	 *
+	 * @throws RouterException
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->redirect('/about', '/');
+
+		$this->get('/', [$this, 'onGetAbout']);
+		$this->get('/projects', [$this, 'onGetProjects']);
+		$this->get('/projects/weather-icons', [$this, 'onGetProjectsWeatherIcons']);
+		$this->get('/contact', [$this, 'onGetContact']);
+	}
+
+	/**
+	 * Invoked when GET /about is requested.
+	 *
+	 * @return string
+	 * @throws RouterException
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 * @internal
+	 */
+	public final function onGetAbout(): string
+	{
+		return $this->render('@bas-website/content/about');
+	}
+
+	/**
+	 * Invoked when GET /projects is requested.
+	 *
+	 * @return string
+	 * @throws RouterException
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 * @internal
+	 */
+	public final function onGetProjects(): string
+	{
+		return $this->render('@bas-website/content/projects');
+	}
+
+	/**
+	 * Invoked when GET /projects/weather-icons is requested.
+	 *
+	 * @return string
+	 * @throws RouterException
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 * @internal
+	 */
+	public final function onGetProjectsWeatherIcons(): string
+	{
+		return $this->render('@bas-website/content/weather-icons', [
+			'icons' => [
+				['id' => 'clear-day', 'name' => 'Clear (day)'],
+				['id' => 'clear-night', 'name' => 'Clear (night)'],
+				['id' => 'cloudy', 'name' => 'Cloudy'],
+				['id' => 'drizzle', 'name' => 'Drizzle'],
+				['id' => 'hail', 'name' => 'Hail'],
+				['id' => 'mist', 'name' => 'Mist'],
+				['id' => 'partly-cloudy-day', 'name' => 'Partly cloudy (day)'],
+				['id' => 'partly-cloudy-day-drizzle', 'name' => 'Partly cloudy drizzle (day)'],
+				['id' => 'partly-cloudy-day-hail', 'name' => 'Partly cloudy hail (day)'],
+				['id' => 'partly-cloudy-day-rain', 'name' => 'Partly cloudy rain (day)'],
+				['id' => 'partly-cloudy-day-snow', 'name' => 'Partly cloudy snow (day)'],
+				['id' => 'partly-cloudy-night', 'name' => 'Partly cloudy (night)'],
+				['id' => 'partly-cloudy-night-drizzle', 'name' => 'Partly cloudy drizzle (night)'],
+				['id' => 'partly-cloudy-night-hail', 'name' => 'Partly cloudy hail (night)'],
+				['id' => 'partly-cloudy-night-rain', 'name' => 'Partly cloudy rain (night)'],
+				['id' => 'partly-cloudy-night-snow', 'name' => 'Partly cloudy snow (night)'],
+				['id' => 'rain', 'name' => 'Rain'],
+				['id' => 'snow', 'name' => 'Snow'],
+				['id' => 'thunderstorms', 'name' => 'Thunderstorms'],
+				['id' => 'tornado', 'name' => 'Tornado'],
+				['id' => 'wind', 'name' => 'Wind']
+			]
+		]);
+	}
+
+	/**
+	 * Invoked when GET /contact is requested.
+	 *
+	 * @return string
+	 * @throws RouterException
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 * @internal
+	 */
+	public final function onGetContact(): string
+	{
+		return $this->render('@bas-website/content/contact');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.5.0
+	 */
+	public function onExecute(AbstractRoute $route, Context $context): void
+	{
+		parent::onExecute($route, $context);
+
+		$this->getCappuccino()->addGlobal('is_webp_supported', Environment::isWebPSupported());
+
+		$this->getAssets()->clear();
+	}
+
+	/**
+	 * {@inheritdoc}
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.5.0
+	 */
+	public function onNotFound(string $requestPath, Context $context): bool
+	{
+		if (StringUtil::startsWith($_SERVER['REQUEST_URI'] ?? '', '/robots.txt'))
+			return false;
+
+		$context->setResponse($context->getResponseImplementation(), $this->render('@bas-website/content/404', [
+			'path' => $_SERVER['REQUEST_URI']
+		]));
+
+		$context->setResponseCode(ResponseCode::NOT_FOUND);
+
+		return true;
+	}
+
+}
---
+++ src/Bas/Website/WebsiteModule.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Copyright (c) 2019 - Bas Milius <bas@mili.us>
+ *
+ * This file is part of the Latte Framework package.
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Bas\Website;
+
+use Bas\Website\Router\RootRouter;
+use Cappuccino\Error\LoaderError;
+use Columba\Router\RouterException;
+use Latte\Core\Helper\Environment;
+use Latte\Core\Module\AbstractModule;
+
+/**
+ * Class WebsiteModule
+ *
+ * @author Bas Milius <bas@mili.us>
+ * @package Bas\Website
+ * @since 1.0.0
+ */
+final class WebsiteModule extends AbstractModule
+{
+
+	/**
+	 * {@inheritdoc}
+	 * @throws LoaderError
+	 * @throws RouterException
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	public final function onLoad(): void
+	{
+		if (Environment::isAdminOrAPI() || $this->getApplication()->getRequest()->pathName() === '/manifest.json')
+			return;
+
+		$this->getCappuccino()->addPath($this->getDirectory('resource/view'), 'bas-website');
+		$this->getRouter()->all('/', RootRouter::class);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	public final function onInstall(): void
+	{
+	}
+
+	/**
+	 * {@inheritdoc}
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	public final function onUninstall(): void
+	{
+	}
+
+	/**
+	 * {@inheritdoc}
+	 * @author Bas Milius <bas@mili.us>
+	 * @since 1.0.0
+	 */
+	public static function shouldUseModule(): bool
+	{
+		return !Environment::isAdminOrAPI();
+	}
+
+}
